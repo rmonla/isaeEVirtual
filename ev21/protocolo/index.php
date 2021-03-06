34e7037b6b04538eff4c84ebadda2f124fab192b
Ricardo MONLA <rmonla@gmail.com>
2021-03-06 00:54:16 -0300

    Actu 210306 0053

---
+++ ev21/protocolo/index.php
@@ -0,0 +1,10 @@
+<?php
+	
+	$newURL = 'https://docs.google.com/document/d/1gbDEs-QOZbF9e5kOGeW0RPvPPcpDnzNiaPX3yY_LnFE/edit?usp=sharing';
+	// $newURL = 'http://www.frlr.utn.edu.ar';
+	
+	header('Location: '.$newURL);
+
+	die();
+
+?>
