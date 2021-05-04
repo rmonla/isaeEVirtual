f5b929c3ab25be2716491b8d91ada028785efc4d
Ricardo MONLA <rmonla@gmail.com>
2021-05-04 02:17:32 -0300

    2.2 => Divide bdAulas en Grupos y Aulas.

--- ev21/aulas/_main/bdAulas.php
+++ ev21/aulas/_main/bdAulas.php
@@ -3,34 +3,52 @@
  * bdAulas.php es el archivo con la información de la base de datos de las aulas virtuales.
  */
 
-	$bdAULAs = [ // BD de IDs de Aulas Zoom. 
-		"21P1A"  => ['89909697836', '1er Grado [A] - ISAE 2021'],
-		"21P1B"  => ['84689282612', '1er Grado [B] - ISAE 2021'],
-		"21P2A"  => ['84309965356', '2do Grado [A] - ISAE 2021'],
-		"21P2B"  => ['85765817321', '2do Grado [B] - ISAE 2021'],
-		"21P3"   => ['86051147626', '3er Grado - ISAE 2021'],
-		"21P4"   => ['87963509629', '4to Grado - ISAE 2021'],
-		"21P5"   => ['86414208695', '5to Grado - ISAE 2021'],
-		"21P6"   => ['87522044907', '6to Grado - ISAE 2021'],
-		"21P7"   => ['89237904211', '7mo Grado - ISAE 2021'],
+	$bdAULAs = [ // BD Aulas. 
+		'PRIMARIA'   => [
+			"21P1A"  => ['89909697836', '1er Grado [A] - ISAE 2021'],
+			"21P1B"  => ['84689282612', '1er Grado [B] - ISAE 2021'],
+			"21P2A"  => ['84309965356', '2do Grado [A] - ISAE 2021'],
+			"21P2B"  => ['85765817321', '2do Grado [B] - ISAE 2021'],
+			"21P3"   => ['86051147626', '3er Grado - ISAE 2021'],
+			"21P4"   => ['87963509629', '4to Grado - ISAE 2021'],
+			"21P5"   => ['86414208695', '5to Grado - ISAE 2021'],
+			"21P6"   => ['87522044907', '6to Grado - ISAE 2021'],
+			"21P7"   => ['89237904211', '7mo Grado - ISAE 2021']
+		],
 		
-		"21S1"   => ['83050120319', '1er Año - ISAE 2021'],
-		"21S2"   => ['84721387672', '2do Año - ISAE 2021'],
-		"21S3"   => ['83690447524', '3er Año - ISAE 2021'],
-		"21S4"   => ['83751299693', '4to Año - ISAE 2021'],
-		"21S5"   => ['89085143676', '5to Año - ISAE 2021'],
-		
-		"nsinfo" => ['84946148032', 'Mod. Informática - ISAE 2021'],
-		"nsnatu" => ['83138483225', 'Mod. Cs. Naturales - ISAE 2021'],
-
-		"aID"    => ["aZOOM", "aNOMBRE"]
+		'SECUNDARIA' => [
+			"21S1"   => ['83050120319', '1er Año - ISAE 2021'],
+			"21S2"   => ['84721387672', '2do Año - ISAE 2021'],
+			"21S3"   => ['83690447524', '3er Año - ISAE 2021'],
+			"21S4"   => ['83751299693', '4to Año - ISAE 2021'],
+			"21S5"   => ['89085143676', '5to Año - ISAE 2021'],
+			"nsinfo" => ['84946148032', 'Mod. Informática - ISAE 2021'],
+			"nsnatu" => ['83138483225', 'Mod. Cs. Naturales - ISAE 2021']
+		]
 	];
 
-	/**
-	 * Genera el html de enlace de un aula a partir de in ID que se encuentra en la base de datos.
-	 * @param  string $aID ID del Aula
-	 * @return string      HTML de acceso al Aula.
-	 */
+	function getAulas(){
+		global $bdAULAs;
+		$htmlAulas = ''; 
+		$htmlGrupos = ''; 
+		
+		foreach ($bdAULAs as $nivel => $aulas) {
+			
+			foreach ($aulas as $aID => $aDats) {
+				//Datos del Aula.
+					list( $aZOOM, $aNOMBRE ) = $aDats;
+				// Enlace del Aula.
+					$aURL = "https://zoom.us/j/$aZOOM";
+					$aLNK = "<a target='_blank' href='$aURL'>$aNOMBRE</a>";
+				// Carga el Aula.
+					$htmlAulas .= "<p class='align-center' mbr-fonts-style panel-text display-7'>$aLNK</p>";
+			}
+			// Carga el Grupo.
+				$htmlGrupos .= getGrupo($nivel, $nivel, $htmlAulas);
+		}
+	return $htmlGrupos;		
+	}
+	
 	function getAula($aID){
 		global $bdAULAs;
 		
@@ -46,13 +64,6 @@
 		return "<p class='align-center' mbr-fonts-style panel-text display-7'>$aLNK</p>";
 	}
 	
-/**
- * Genera el html del grupo de Aulas.
- * @param  string $grupo  ID del grupo
- * @param  string $titulo TITULO para mostrar del Grupo.
- * @param  string $aulas  HTML de las Aulas.
- * @return html           HTML del Grupo.
- */
 function getGrupo($grupo='', $titulo='', $aulas=''){
     $grupo2 = $grupo . '_0';
     
@@ -75,30 +86,5 @@ function getGrupo($grupo='', $titulo='', $aulas=''){
 HTML;
   }
 
-function getGrupo2($grupo='', $titulo='', $aulas=''){
-    $grupo2 = $grupo . '_0';
-    
-    return <<<HTML
-    <div class="card"> <!-- $titulo -->
-        <div class="card-header" role="tab" id="headingDos">
-            <a role="button" class="panel-title collapsed text-black" data-toggle="collapse" data-core="" href="#$grupo2" aria-expanded="false" aria-controls="$grupo">
-                <h4 class="mbr-fonts-style display-5">
-                    <span class="sign mbr-iconfont mbri-arrow-down inactive"></span>
-                    $titulo
-                  </h4>
-              </a>
-          </div>
-        <div id="$grupo2" class="panel-collapse noScroll collapse " role="tabpanel" aria-labelledby="headingDos" data-parent="#bootstrap-accordion_0">
-            <div class="panel-body p-4">
-              $aulas
-            </div>
-          </div>
-    </div>
-HTML;
-  }
-
-	// echo '<pre>'.print_r( $bdZoomIDs, true ).'</pre>';
-
-	// exit;
 
  ?>
---
+++ ev21/aulas/index_v2.0.php
@@ -0,0 +1,121 @@
+<?php 
+    include_once '_rmApp.php';
+    include_once '_main/fxs.php';
+
+?>
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.11.6, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.11.6, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <!-- <link rel="shortcut icon" href="assets/images/dticAulaVirtual.png" type="image/x-icon"> -->
+  <meta name="description" content="">
+  <meta http-equiv="Refresh" content="150"/>
+  
+  <title>Aulas Virtuales - ISAE</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+</head>
+<body>
+  <section class="accordion1 cid-rKzC2nyguD" id="accordion1-0">
+    <div class="container">
+        <div class="media-container-row">
+            <div class="col-12 col-md-8">
+                <div class="section-head text-center space30">
+                    <h2 class="mbr-section-title pb-5 mbr-fonts-style display-2"
+                    >Aulas Virtuales - <a href='http://www.isae.edu.ar'>ISAE</a></h2>
+                     <div class="align-right">
+                      <?php echo "<a href='".APPPUB."'>".APPVER."</a>"; ?></div>
+                </div>
+                <div class="clearfix"></div>
+                <div id="bootstrap-accordion_0" class="panel-group accordionStyles accordion" role="tablist" aria-multiselectable="true">
+                    
+                    <div class="card"> <!-- Primaria -->
+                        <div class="card-header" role="tab" id="headingOne">
+                            <a role="button" class="panel-title collapsed text-black" data-toggle="collapse" data-core="" href="#Primaria_0" aria-expanded="false" aria-controls="Primaria">
+                                <h4 class="mbr-fonts-style display-5">
+                                    <span class="sign mbr-iconfont mbri-arrow-down inactive"></span>
+                                    Nivel Primario
+                                  </h4>
+                              </a>
+                          </div>
+                        <div id="Primaria_0" class="panel-collapse noScroll collapse " role="tabpanel" aria-labelledby="headingOne" data-parent="#bootstrap-accordion_0">
+                            <div class="panel-body p-4">
+                                
+                                <?php  
+                                  $lstA = ['21P1A', '21P1B', '21P2A', '21P2B', '21P3', '21P4', '21P5', '21P6', '21P7']; 
+
+                                  foreach ($lstA as $a) echo getAula($a);
+                                ?>
+
+                              </div>
+                          </div>
+                    </div>
+                    
+                    <div class="card"> <!-- Nivel Secundario -->
+                        <div class="card-header" role="tab" id="headingOne">
+                            <a role="button" class="panel-title collapsed text-black" data-toggle="collapse" data-core="" href="#Secundaria_0" aria-expanded="false" aria-controls="Secundaria">
+                                <h4 class="mbr-fonts-style display-5">
+                                    <span class="sign mbr-iconfont mbri-arrow-down inactive"></span>
+                                    Nivel Secundario
+                                  </h4>
+                              </a>
+                          </div>
+                        <div id="Secundaria_0" class="panel-collapse noScroll collapse " role="tabpanel" aria-labelledby="headingOne" data-parent="#bootstrap-accordion_0">
+                            <div class="panel-body p-4">
+                                
+                                <?php  
+                                  $lstA = ['21S1', '21S2', '21S3', '21S4', '21S5', 'nsinfo', 'nsnatu']; 
+
+                                  foreach ($lstA as $a) echo getAula($a);
+                                ?>
+
+                              </div>
+                          </div>
+                    </div>
+
+                              </div>
+                          </div>
+                    </div>
+                    
+                </div>
+              </div>
+          </div>
+      </div>
+    </section>
+
+
+  <section class="engine"><a href="https://mobirise.info/a">online website builder</a></section>
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/mbr-switch-arrow/mbr-switch-arrow.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-175446358-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-175446358-1');
+
+
+    
+  </script>
+  
+</body>
+</html>
